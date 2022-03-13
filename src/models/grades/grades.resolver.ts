d2eb555de2300e7823a8c46a62e66119729e954b
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-13 12:53:06 +0100

    add create level utation

--- src/models/grades/grades.resolver.ts
+++ src/models/grades/grades.resolver.ts
@@ -1,7 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateGradeInputs } from './dto/create-grade.inputs';
+import { Grade } from './Grade';
 import { GradesService } from './grades.service';
 
 @Resolver()
 export class GradesResolver {
   constructor(private readonly gradesService: GradesService) {}
+
+  @Mutation(() => Grade)
+  createGrade(@Args('inputs') inputs: CreateGradeInputs) {
+    return this.gradesService.createGrade(inputs);
+  }
 }
