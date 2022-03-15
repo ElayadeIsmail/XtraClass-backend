d1341a560d0fb3a3d87be6579223be96b7bf4f6d
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-15 12:06:43 +0100

    add create course

--- src/models/courses/courses.module.ts
+++ src/models/courses/courses.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CoursesService } from './courses.service';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 import { CoursesResolver } from './courses.resolver';
+import { CoursesService } from './courses.service';
 
 @Module({
-  providers: [CoursesResolver, CoursesService]
+  providers: [CoursesResolver, CoursesService, PrismaService],
 })
 export class CoursesModule {}
--- src/models/courses/courses.service.ts
+++ src/models/courses/courses.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma/prisma.service';
+import { CreateCourseInput } from './dto/create-course-inputs';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCourse(inputs: CreateCourseInput) {
+    const { gradeId, levelId, subjectId, specializationId } = inputs;
+    const gradePromise = this.prisma.grade.findUnique({
+      where: {
+        id: gradeId,
+      },
+    });
+    const levelPromise = this.prisma.level.findUnique({
+      where: {
+        id: levelId,
+      },
+    });
+    const subjectPromise = this.prisma.subject.findUnique({
+      where: {
+        id: subjectId,
+      },
+    });
+    const specializationsPromise = this.prisma.specialization.findUnique({
+      where: {
+        id: specializationId,
+      },
+    });
+    const PromisesResult = await Promise.all([
+      gradePromise,
+      levelPromise,
+      subjectPromise,
+      specializationsPromise,
+    ]);
+    const fields = ['grade', 'level', 'subject', 'specialization'];
+    PromisesResult.forEach((item, index) => {
+      if (!item) {
+        throw new BadRequestException(`${fields[index]} does not exist`);
+      }
+    });
+    return this.prisma.course.create({
+      data: inputs,
+    });
+  }
+}
