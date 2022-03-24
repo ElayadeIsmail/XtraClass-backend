46c795e033da42b4c4cf76295b333fed791723b9
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-24 20:17:40 +0100

    add specialization resolver

--- src/models/specializations/specializations.resolver.ts
+++ src/models/specializations/specializations.resolver.ts
@@ -1,7 +1,35 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Specialization } from './Specialization';
 import { SpecializationsService } from './specializations.service';
 
 @Resolver()
 export class SpecializationsResolver {
-  constructor(private readonly specializationsService: SpecializationsService) {}
+  constructor(
+    private readonly specializationsService: SpecializationsService,
+  ) {}
+
+  @Mutation(() => Specialization)
+  async createSpecialization(
+    @Args('name') name: string,
+  ): Promise<Specialization> {
+    return this.specializationsService.create(name);
+  }
+
+  @Query(() => Specialization)
+  async findOneSpecialization(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Specialization> {
+    return this.specializationsService.findOne(id);
+  }
+  @Query(() => [Specialization])
+  async findManySpecialization(): Promise<Specialization[]> {
+    return this.specializationsService.find();
+  }
+
+  @Mutation(() => Specialization)
+  async deleteSpecialization(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Specialization> {
+    return this.specializationsService.delete(id);
+  }
 }
