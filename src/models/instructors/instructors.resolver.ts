0280bb02aa69be216e879ff6f9c034b663c68721
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-27 15:34:53 +0000

    add instructor resolver

--- src/models/instructors/instructors.resolver.ts
+++ src/models/instructors/instructors.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateInstructorInputs } from './dtos/create-instructor-inputs';
 import { Instructor } from './Instructor';
 import { InstructorsService } from './instructors.service';
@@ -10,5 +10,19 @@ export class InstructorsResolver {
   @Mutation(() => Instructor)
   async createInstructor(@Args('inputs') inputs: CreateInstructorInputs) {
     return this.instructorsService.createInstructor(inputs);
+  }
+
+  @Query(() => Instructor)
+  findOneInstructor(@Args('id', { type: () => Int }) id: number) {
+    return this.instructorsService.findOne(id);
+  }
+  @Query(() => [Instructor])
+  findInstructors() {
+    return this.instructorsService.find();
+  }
+
+  @Mutation(() => Instructor)
+  deleteInstructor(@Args('id', { type: () => Int }) id: number) {
+    return this.instructorsService.delete(id);
   }
 }
