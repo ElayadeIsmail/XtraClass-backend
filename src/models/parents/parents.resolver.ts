27f311d5f1334d1155846643af94bcee0a2cc4b8
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-27 15:50:23 +0000

    add create parent mutations

--- src/models/parents/parents.resolver.ts
+++ src/models/parents/parents.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateParentInput } from './dtos/create-parent.inputs';
+import { Parent } from './Parent';
 import { ParentsService } from './parents.service';
 
 @Resolver()
 export class ParentsResolver {
   constructor(private readonly parentsService: ParentsService) {}
+
+  @Mutation(() => Parent)
+  async createParent(
+    @Args('inputs') inputs: CreateParentInput,
+  ): Promise<Parent> {
+    return this.parentsService.createParent(inputs);
+  }
 }
