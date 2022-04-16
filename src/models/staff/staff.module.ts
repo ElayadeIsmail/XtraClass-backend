b7dfb573626567b068ce4ac1926aa9016b6325a6
ElayadeIsmail <elayadeismail@gmail.com>
2022-04-16 09:50:14 +0000

    more refactor for staff and members

--- src/models/staff/staff.module.ts
+++ src/models/staff/staff.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
 import { StaffResolver } from './staff.resolver';
 import { StaffService } from './staff.service';
 
 @Module({
+  imports: [UsersModule],
   providers: [StaffResolver, StaffService, PrismaService],
 })
 export class StaffModule {}
--- src/models/staff/staff.service.ts
+++ src/models/staff/staff.service.ts
@@ -1,71 +1,29 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Role } from '@prisma/client';
-import slugify from 'slugify';
-import { PasswordManager } from 'src/services/password.service';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { UsersService } from '../users/users.service';
 import { AddStaffInput } from './dto/add-staff-inputs';
 import { Staff } from './Staff';
 
 @Injectable()
 export class StaffService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly userService: UsersService,
+  ) {}
 
   async create(inputs: AddStaffInput): Promise<Staff> {
     const { salary, ...userInput } = inputs;
-    const username = slugify(userInput.firstName + ' ' + userInput.lastName);
+    const validUserInputs = await this.userService.validateUserInputs(
+      userInput,
+    );
 
-    const nameAlreadyExistPromise = this.prisma.user.findUnique({
-      where: {
-        username: username,
-      },
-    });
-    const phoneAlreadyExistPromise = this.prisma.user.findUnique({
-      where: {
-        phone: userInput.phone,
-      },
-    });
-    if (userInput.cin) {
-      const cinAlreadyExist = await this.prisma.user.findUnique({
-        where: {
-          cin: userInput?.cin,
-        },
-      });
-      if (cinAlreadyExist) {
-        throw new BadRequestException('CIN already exist');
-      }
-    }
-    const fullNameAlreadyExistPromise = this.prisma.user.findUnique({
-      where: {
-        fullName: {
-          firstName: userInput.firstName,
-          lastName: userInput.lastName,
-        },
-      },
-    });
-    // check if username of phone number in use
-    const checksResult = await Promise.all([
-      nameAlreadyExistPromise,
-      phoneAlreadyExistPromise,
-      fullNameAlreadyExistPromise,
-    ]);
-    const fields = ['username', 'phone', 'firstName and lastName'];
-    checksResult.forEach((user, idx) => {
-      if (user) {
-        throw new BadRequestException(`${fields[idx]} Already exist`);
-      }
-    });
-
-    const generatedPassword = PasswordManager.generatePassword();
-    const hashedPassword = await PasswordManager.hash(generatedPassword);
     return this.prisma.staff.create({
       data: {
         salary,
         user: {
           create: {
-            ...userInput,
-            username,
-            password: hashedPassword,
-            generatedPassword,
+            ...validUserInputs,
             role: Role.Staff,
           },
         },
